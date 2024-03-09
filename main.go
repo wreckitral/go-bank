aba9ae74eeafe2e43a11c5a8c7a037ccb6890d53
Defhanaya Sofhiea <defhanayasofhiea@gmail.com>
2024-03-09 17:57:46 +0700

    chore(main): initialized new database storage
    call the NewPostgresStore function to declare storage and initialized
    the storage using Init function then using it on updated NewAPIServer

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
+import (
+	"log"
+)
+
 func main() {
-	server := NewAPIServer(":7777")
+	store, err := NewPostgresStore()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := store.Init(); err != nil {
+		log.Fatal(err)
+	}
+
+	server := NewAPIServer(":7777", store)
 
 	server.Run()
 }

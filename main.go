ba92ee9b7dd0e399b82a7be3633e2d0e2e5ff639
Defhanaya Sofhiea <defhanayasofhiea@gmail.com>
2024-03-16 13:32:35 +0700

    feat(main): create seeding function

--- main.go
+++ main.go
@@ -1,10 +1,34 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 )
 
+func seedAccount(store Storage, fname, lname, pw string) *Account {
+	acc, err := NewAccount(fname, lname, pw)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := store.CreateAccount(acc); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("new account => ", acc.Number)
+
+	return acc
+}
+
+func seedAccounts(s Storage) {
+	seedAccount(s, "udin", "jamal", "siti")
+}
+
 func main() {
+	seed := flag.Bool("seed", false, "seed the db")
+	flag.Parse()
+
 	store, err := NewPostgresStore()
 	if err != nil {
 		log.Fatal(err)
@@ -14,6 +38,10 @@ func main() {
 		log.Fatal(err)
 	}
 
+	if *seed {
+		fmt.Println("seeding the database")
+		seedAccounts(store)
+	}
 	server := NewAPIServer(":7777", store)
 
 	server.Run()
